99c9e5edb1e60d0863da4c8f203b56192d888713
Andrew Lisowski <lisowski54@gmail.com>
2024-11-08 23:17:34 -0800

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          data-domain="bluesky-labelers.io"
+          src="https://plausible.io/js/script.js"
+        />
+      </Head>
       <body className="antialiased bg-gray-app text-gray-normal">
         <Main />
         <NextScript />
